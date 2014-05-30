2ce99da43030b7f7d94fdb91b228289f5001817e
drw86312 <david.warner4@gmail.com>
2014-05-30 13:06:09 -0500

    Initial Commit

---
+++ Wild Kingdom/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wild Kingdom
+//
+//  Created by David Warner on 5/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
